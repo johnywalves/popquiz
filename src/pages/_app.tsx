49d95b66028b8f8258d5e2970982eecf815d1493
Johny W. Alves <johnywalves@gmail.com>
2021-01-28 20:41:21 -0300

    :wrench: Configuração de nome para o Pop Music Quiz

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,13 +6,13 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Pop Music Quiz</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+          content="Quiz sobre música popular atráves dos anos"
         />
       </Head>
       <GlobalStyles />
